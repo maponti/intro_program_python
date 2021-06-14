c89236ffb765689b515dbd7c66ac212064fc157a
maponti <ponti@usp.br>
2021-06-14 17:18:15 -0300

    TN 6

---
+++ ipe_06_modulo.py
@@ -0,0 +1,11 @@
+# Módulo Python
+
+year = 2021
+professor = 'Moacir A. Ponti'
+
+def about():
+    print("Esse é um módulo para exemplificar a criação de módulos Python")
+    print(100*"=")
+    print("scc0124 -", str(year))
+    print("USP São Carlos")
+    print("Prof.", professor)
